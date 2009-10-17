c41af2f35781993dee7b774a16e75791de37af13
caron.louis <caron.louis@0745b4e8-b968-11de-9846-1b058f2a8eb1>
2009-10-17 14:04:12 +0000

    Add headers

--- src/common/Flash.h
+++ src/common/Flash.h
@@ -1,12 +1,24 @@
 /*
- * Flash.h
+ * Internal Flash related API
  *
- *  Created on: Oct 15, 2009
- *      Author: lcaron
+ *    Copyright (C) 2009 Louis Caron
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef FLASH_H_
-#define FLASH_H_
+#ifndef _FLASH_H_
+#define _FLASH_H_
 
 /**
  * Start the internal flash regulators.
@@ -15,4 +27,4 @@ extern void
 FlashStartReg(void);
 
 
-#endif /* FLASH_H_ */
+#endif // _FLASH_H_
