4db586f994f6b9fa89a99c253e73fbb6033d36cf
caron.louis <caron.louis@0745b4e8-b968-11de-9846-1b058f2a8eb1>
2009-10-17 14:03:11 +0000

    Add compiler related features

---
+++ src/compiler/gnuarm/compiler.h
@@ -0,0 +1,35 @@
+/*
+ * Compiler related features
+ *
+ *    Copyright (C) 2009 Louis Caron
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef _COMPILER_H_
+#define _COMPILER_H_
+
+#ifndef __GNUC__
+#error "File only included with ARM GCC"
+#endif
+
+/// define the force inlining attribute for this compiler
+#define __INLINE static __attribute__((__always_inline__)) inline
+
+/// define the IRQ handler attribute for this compiler
+#define __IRQ __attribute__((__interrupt__("IRQ")))
+
+/// define the FIQ handler attribute for this compiler
+#define __FIQ __attribute__((__interrupt__("FIQ")))
+
+#endif // _COMPILER_H_
