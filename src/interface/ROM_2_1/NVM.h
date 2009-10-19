dae147ff9ac35f118f3342f21f6b3428092779fb
caron.louis <caron.louis@0745b4e8-b968-11de-9846-1b058f2a8eb1>
2009-10-19 08:29:17 +0000

    Add include dependency

--- src/interface/ROM_2_1/NVM.h
+++ src/interface/ROM_2_1/NVM.h
@@ -18,6 +18,7 @@
 #ifndef _NVM_DRIVER_H_
 #define _NVM_DRIVER_H_
 
+#include <stdint.h>
 /************************************************************************************
 *************************************************************************************
 * Public macros
@@ -61,7 +62,7 @@ typedef enum
 typedef enum
 {
   gNvmInternalInterface_c,
-  gNvmExternalInterface_c, 
+  gNvmExternalInterface_c,
   gNvmInterfaceMax_c
 } nvmInterface_t;
 
@@ -83,16 +84,16 @@ typedef enum
 
 /************************************************************************************
 * This function executes a search for the known NVM types
-*   
+*
 * Interface assumptions:
 *   None
-*   
+*
 * Return value:
 * nvmErr
 *   gNvmErrInvalidInterface_c  : if nvmInterface is not one of the two valid values(gNvmInternalInterface_c or gNvmExternalInterface_c)
 *   gNvmErrInvalidPointer_c    : if pNvmType is NULL
 *   gNvmErrNoError_c           : if nvmInterface is one of the two valid values
-* 
+*
 * *pNvmType
 *   gNvmType_SST_c             : SST   NVM was found
 *   gNvmType_ST_c              : ST    NVM was found
@@ -110,10 +111,10 @@ extern nvmErr_t NVM_Detect(nvmInterface_t nvmInterface,nvmType_t* pNvmType);
 * Reads numBytes of data from the flash. The address is used initially, but any
 * subsequent reads are done in "continous-read" mode, without setting the address
 * again. The read data is copied into the memory location referred to by pDest.
-*   
+*
 * Interface assumptions:
 *   The address uses only 24 bits.
-*   
+*
 * Return value:
 *
 * nvmErr_t
@@ -121,7 +122,7 @@ extern nvmErr_t NVM_Detect(nvmInterface_t nvmInterface,nvmType_t* pNvmType);
 *   gNvmErrInvalidNvmType_c        : if nvmType is not one of the three known values(gNvmType_SST_c,gNvmType_ST_c,gNvmType_ATM_c)
 *   gNvmErrAddressSpaceOverflow_c  : if (address +numBytes-1 )> gNvmMaxAddress_c
 *   gNvmErrInvalidPointer_c        : if  pDest == NULL
-*   gNvmErrRestrictedArea_c        : if  nvmInterface = gNvmInternalInterface_c and the address space reachs last sector 
+*   gNvmErrRestrictedArea_c        : if  nvmInterface = gNvmInternalInterface_c and the address space reachs last sector
 *   gNvmErrNoError_c               : in rest
 * Revision history:
 *   date      Author    Comments
@@ -133,20 +134,20 @@ extern nvmErr_t NVM_Read(nvmInterface_t nvmInterface , nvmType_t nvmType , void 
 
 /************************************************************************************
 * Erase a set of sectors, identified by the sector_bitfield.
-*   
-* for  nvmType = gNvmType_SST_c NVM 
+*
+* for  nvmType = gNvmType_SST_c NVM
 *         Sector size = 4096. Number of sectors = 32 .
 *         Each bit of the sectorBitfield represents a sector.Bit 0 represents sector 0... Bit 31 represents sector 31
-* 
+*
 * for  nvmType = gNvmType_ST_c or nvmType = gNvmType_ATM_c
-*         Sector size = 32768. Number of sectors = 4 
+*         Sector size = 32768. Number of sectors = 4
 *         Only the least 4  significant bits represents a sector.Bit 0 represents sector 0... Bit 3 represents sector 3
-*   
+*
 * Return value:
 *   nvmErr
 *     gNvmErrInvalidInterface_c      : if nvmInterface is not one of the two valid values(gNvmInternalInterface_c or gNvmExternalInterface_c)
-*     gNvmErrInvalidNvmType_c        : if nvmType is not one of the three known values(gNvmType_SST_c,gNvmType_ST_c,gNvmType_ATM_c) 
-*     gNvmErrRestrictedArea_c        : if the last sector is set to be erased  
+*     gNvmErrInvalidNvmType_c        : if nvmType is not one of the three known values(gNvmType_SST_c,gNvmType_ST_c,gNvmType_ATM_c)
+*     gNvmErrRestrictedArea_c        : if the last sector is set to be erased
 *     gNvmErrNoError_c               : in rest
 * Revision history:
 *   date      Author    Comments
@@ -160,53 +161,53 @@ extern nvmErr_t NVM_Erase(nvmInterface_t nvmInterface, nvmType_t nvmType ,uint32
 *
 * Interface assumptions:
 *   The address uses only 24 bits.
-*   
+*
 * Return value:
 *   nvmErr
 *     gNvmErrInvalidInterface_c      : if nvmInterface is not one of the two valid values(gNvmInternalInterface_c or gNvmExternalInterface_c)
 *     gNvmErrInvalidNvmType_c        : if nvmType is not one of the three known values(gNvmType_SST_c,gNvmType_ST_c,gNvmType_ATM_c)
 *     gNvmErrAddressSpaceOverflow_c  : if (address +numBytes-1 )> gNvmMaxAddress_c
-*     gNvmErrRestrictedArea_c        : if  nvmInterface = gNvmInternalInterface_c and the address space reachs last sector 
+*     gNvmErrRestrictedArea_c        : if  nvmInterface = gNvmInternalInterface_c and the address space reachs last sector
 *     gNvmErrBlankCheckError_c       : if at lest one of the location to write is not blank(0xff)
-*     gNvmErrWriteProtect_c          : if the NVM cannot be enabled to be write   
+*     gNvmErrWriteProtect_c          : if the NVM cannot be enabled to be write
 *     gNvmErrVerifyError_c           : if after the write operation at least one location in NVM is different from it's intended value
 *     gNvmErrNoError_c               : in rest
 * Revision history:
 *   date      Author    Comments
 *   ------    ------    --------
 * 07.11.2007   MD
-* 
+*
 ************************************************************************************/
 extern nvmErr_t NVM_Write(nvmInterface_t nvmInterface, nvmType_t nvmType ,void *pSrc, uint32_t address, uint32_t numBytes);
 
 /************************************************************************************
 * Verifies  numBytes of data from the address address in flash to be equal with the data referred to by pSrc in RAM .
-*   
+*
 * Interface assumptions:
 *   The address uses only 24 bits.
-*   
+*
 * Return value:
 *   nvmErr
 *     gNvmErrInvalidInterface_c      : if nvmInterface is not one of the two valid values(gNvmInternalInterface_c or gNvmExternalInterface_c)
 *     gNvmErrInvalidNvmType_c        : if nvmType is not one of the three known values(gNvmType_SST_c,gNvmType_ST_c,gNvmType_ATM_c)
 *     gNvmErrAddressSpaceOverflow_c  : if (address +numBytes-1 )> gNvmMaxAddress_c
 *     gNvmErrInvalidPointer_c        : if pSrc is NULL
-*     gNvmErrVerifyError_c           : if at least one location in NVM is different from it's corespondind location in RAM 
+*     gNvmErrVerifyError_c           : if at least one location in NVM is different from it's corespondind location in RAM
 *     gNvmErrNoError_c               : in rest
 * Revision history:
 *   date      Author    Comments
 *   ------    ------    --------
 * 07.11.2007   MD
-* 
+*
 ************************************************************************************/
 extern nvmErr_t NVM_Verify(nvmInterface_t nvmInterface, nvmType_t nvmType ,void *pSrc, uint32_t address, uint32_t numBytes);
 
 /************************************************************************************
 * Verifies  numBytes of data from the address address in NVM to be blank .
-*   
+*
 * Interface assumptions:
 *   The address uses only 24 bits.
-*   
+*
 * Return value:
 *   nvmErr
 *     gNvmErrInvalidInterface_c      : if nvmInterface is not one of the two valid values(gNvmInternalInterface_c or gNvmExternalInterface_c)
@@ -219,7 +220,7 @@ extern nvmErr_t NVM_Verify(nvmInterface_t nvmInterface, nvmType_t nvmType ,void 
 *   date      Author    Comments
 *   ------    ------    --------
 * 07.11.2007   MD
-* 
+*
 ************************************************************************************/
 extern nvmErr_t NVM_BlankCheck(nvmInterface_t nvmInterface, nvmType_t nvmType , uint32_t address, uint32_t numBytes);
 
