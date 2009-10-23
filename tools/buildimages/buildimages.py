d34f0e97dafbff7747cc8e167be652b363b4bfa8
caron.louis <caron.louis@0745b4e8-b968-11de-9846-1b058f2a8eb1>
2009-10-23 12:59:45 +0000

    Enhance tool output

--- tools/buildimages/buildimages.py
+++ tools/buildimages/buildimages.py
@@ -12,11 +12,15 @@ import common.bytes
 
 usage_doc ="""
 Synopsis:
-    buildimages.py [--help] [-o radix] elffile
+    buildimages.py [-v|--verbose] [-h|--help] [-o radix] elffile
 
+       -h
        --help: self explanatory
-       -o radix: radix of the output images name, defaults to _ram.bin and _flash.bin
-          if radix = foo -> foo_ram.bin and foo_flash.bin
+       -v
+       --verbose: self explanatory
+       -o radix: radix of the output images name, defaults to 'image',
+                 generating image_ram.bin and image_flash.bin
+                 if radix is foo -> generate foo_ram.bin and foo_flash.bin
        elffile : ELF object file containing the loadable to generate images for
 """
 
@@ -24,40 +28,48 @@ def usage():
     print usage_doc
 
 def main():
-
-    # radix not specified
-    radix = ""
+    # default verbose mode
+    verbose = False
+    # default radix
+    radix = "image"
 
     # parse the command line
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "o:", ["help", ])
+        opts, args = getopt.getopt(sys.argv[1:], "vho:", ["help", "verbose"])
     except getopt.GetoptError:
+        print("Unsupported option")
         # print help information and exit:
         usage()
         sys.exit(-1)
 
     for o, a in opts:
-        if o == "--help":
+        if o == "--help" or o == "-h":
             usage()
             sys.exit(-1)
         if o == "-o":
             radix = a
+        if o == "-v" or o == "--verbose":
+            verbose = True
 
     # sanity check
     if len(args) != 1:
-        print("Number of parameters not supported (%s)"%(args,))
+        print("ELF object file not provided (%s)"%(args,))
         sys.exit(-1)
 
     # get the elf file name
     elffile = args[0]
     
-    # read the content of the entire elf file
-    fid = open(elffile, 'rb')
-    content = fid.read()
-    fid.close()
+    try:
+        # read the content of the entire elf file
+        fid = open(elffile, 'rb')
+        content = fid.read()
+        fid.close()
+    except:
+        print("Can't read ELF object file (%s)"%(elffile,))
+        sys.exit(-1)
 
-    # print the file content
-    print("elf header:\n"+common.bytes.dump(content[0:64]))
+    if verbose:
+        print("ELF File header:\n"+common.bytes.dump(content[0:64]))
     
     # check that the first bytes are the elf magic number
     assert(content[0:4] == "\x7FELF")
@@ -69,15 +81,20 @@ def main():
     # retrieve the program entry
     entry = struct.unpack("L", content[24:28])[0]
     
-    # print the program entry
-    print("entry = 0x%08X"%(entry, ))
+    if verbose:
+        # print the program entry
+        print("ELF File defined entry = 0x%08X"%(entry, ))
     
     # get the program header related variables
     phoff = struct.unpack("L", content[28:32])[0]
     phentsize = struct.unpack("H", content[42:44])[0]
     phnum = struct.unpack("H", content[44:46])[0]
-    print("PH(%d, %d, %d)"%(phoff, phentsize, phnum))
-    assert(phnum != 0)
+    if verbose:
+        print("Program Header list description:\n    First header offset=%d, Header size=%d, Headers number=%d"%(phoff, phentsize, phnum))
+        
+    if phnum == 0:
+        print("ELF object file does not contain any program header")
+        sys.exit(-1)
     
     # initialize the current segment address
     curraddr = 0x400000
@@ -87,27 +104,37 @@ def main():
     for i in range(phnum):
         # extract the program header
         ph = content[phoff+(i*phentsize):phoff+((i+1)*phentsize)]
-        print("program header:\n"+common.bytes.dump(ph))
+        if verbose:
+            print("\n\nProgram header %d:\n"%(i, )+common.bytes.dump(ph))
+            
+        # unpack header
         (type, offset, vaddr, paddr, filesz, memsz) = struct.unpack("LLLLLL", ph[:24])
         # check if this is a loadable segment
         if type == 1:
-            print("  -> current = 0x%08X, code = (0x%08X, 0x%X, 0x%X)"%(curraddr, vaddr, filesz, memsz))
+            if verbose:
+                print("    -> PC=0x%08X, Segment: PC=0x%08X, File Size=0x%X, MemSize=0x%X)"%(curraddr, vaddr, filesz, memsz))
             assert(vaddr >= curraddr)
             assert(memsz >= filesz)
             code += "\0"*(vaddr-curraddr)
             code += content[offset:offset+filesz]
             code += "\0"*(memsz-filesz)
             curraddr += memsz
+        else:
+            if verbose:
+                print("  -> not loadable program")
 
+    
     # generate the flash file
     fid = open(radix+"_flash.bin", 'wb')
     fid.write("OKOK"+struct.pack("L", len(code))+code)
     fid.close()
+    print("... generated '%s': %d bytes"%(fid.name, len(code)))
 
     # generate the RAM file
     fid = open(radix+"_ram.bin", 'wb')
     fid.write(code)
     fid.close()
+    print("... generated '%s': %d bytes"%(fid.name, len(code)))
 
 # main execution
 if __name__ == '__main__':
